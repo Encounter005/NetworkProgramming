3ec26d1596b5fdf2021c7ca74fcdcd5b59737414
Encounter005 <1624221574@qq.com>
2024-07-25 20:35:23 +0800

    MODIFY: 改成循环发送消息

--- executable/Sync_client.cpp
+++ executable/Sync_client.cpp
@@ -20,19 +20,22 @@ int main() {
             return 0;
         }
 
-        cout << "Enter a message: ";
         char request[MAX_LENGTH];
-        cin.getline( request, MAX_LENGTH );
-        size_t request_length = strlen( request );
-        boost::asio::write(
-            sock, boost::asio::buffer( request, request_length ) );
+        while ( true ) {
+            memset(request, 0, sizeof request);
+            cout << "Enter a message: ";
+            cin.getline( request, MAX_LENGTH );
+            size_t request_length = strlen( request );
+            boost::asio::write(
+                sock, boost::asio::buffer( request, request_length ) );
 
-        char reply[MAX_LENGTH];
-        size_t reply_length =
-            boost::asio::read( sock, boost::asio::buffer( reply, request_length ) );
-        cout << "Reply is: ";
-        cout.write( reply, reply_length );
-        cout << endl;
+            char reply[MAX_LENGTH];
+            size_t reply_length = boost::asio::read(
+                sock, boost::asio::buffer( reply, request_length ) );
+            cout << "Reply is: ";
+            cout.write( reply, reply_length );
+            cout << endl;
+        }
 
     } catch ( std::exception &e ) {
         cerr << e.what() << endl;
