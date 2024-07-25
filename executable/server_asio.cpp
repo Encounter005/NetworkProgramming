d333f4cf570c0988d64e22423806a377a2f13896
Encounter005 <1624221574@qq.com>
2024-07-25 20:35:36 +0800

    MODIFY: 修改了运行方式

--- executable/server_asio.cpp
+++ executable/server_asio.cpp
@@ -1,7 +1,12 @@
-#include "../src/lib/AsyncServer.h"
+#include "../src/lib/AsyncServerV2.h"
 
 int main() {
-    boost::asio::io_context ioc;
-    AsyncServer server(ioc, 10086);
+    try {
+        boost::asio::io_context ioc;
+        AsyncServer server( ioc, 10086 );
+        ioc.run();
+    } catch ( std::exception &e ) {
+        std::cout << e.what() << std::endl;
+    }
     return 0;
 }
