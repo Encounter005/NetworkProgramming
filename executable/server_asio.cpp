67e7c51bd4820decd7d6ba6252b9aee60290a270
Encounter005 <1624221574@qq.com>
2024-08-08 09:33:48 +0800

    MODIFY: test new server

--- executable/server_asio.cpp
+++ executable/server_asio.cpp
@@ -1,9 +1,12 @@
-#include "../src/lib/AsyncServerV2.h"
+#include "../src/lib/AsyncServerV3/CServer.h"
 
 int main() {
     try {
         boost::asio::io_context ioc;
-        AsyncServer server( ioc, 10086 );
+        boost::asio::signal_set signals( ioc, SIGINT, SIGTERM );
+        signals.async_wait( [&ioc]( auto, auto ) { ioc.stop(); } );
+        short port = 12345;
+        CServer server( ioc, port );
         ioc.run();
     } catch ( std::exception &e ) {
         std::cout << e.what() << std::endl;
