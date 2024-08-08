997c256f504a3d7097f909a46243efdca5a9293e
Encounter005 <1624221574@qq.com>
2024-08-08 18:28:43 +0800

    MODIFY: add quit lock

--- executable/server_asio.cpp
+++ executable/server_asio.cpp
@@ -1,12 +1,28 @@
-#include "../src/lib/AsyncServerV3/CServer.h"
-
+#include "../src/lib/IOThreadPoolServer/CServer.h"
+#include "lib/IOThreadPoolServer/IOThreadPool.h"
+#include <condition_variable>
+std::mutex quit;
+bool bstop = false;
+std::condition_variable cond_quit;
 int main() {
     try {
+        auto pool = AsioIOThreadPool::GetInstance();
         boost::asio::io_context ioc;
         boost::asio::signal_set signals( ioc, SIGINT, SIGTERM );
-        signals.async_wait( [&ioc]( auto, auto ) { ioc.stop(); } );
+        signals.async_wait( [&ioc, pool]( auto, auto ) {
+            ioc.stop();
+            pool->Stop();
+            std::unique_lock<std::mutex> lock( quit );
+            bstop = true;
+            cond_quit.notify_one();
+        } );
         short port = 12345;
-        CServer server( ioc, port );
+        CServer server( pool->get_io_service(), port );
+
+        {
+            std::unique_lock<std::mutex> lock( quit );
+            cond_quit.wait( lock, [&]() { return bstop; } );
+        }
         ioc.run();
     } catch ( std::exception &e ) {
         std::cout << e.what() << std::endl;
