99d4729b053dcf3930802cb45e2f22ae14a2cabf
Encounter005 <1624221574@qq.com>
2024-08-08 09:34:27 +0800

    ADD: new server

---
+++ src/lib/AsyncServerV3/CServer.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <boost/asio.hpp>
+#include <memory>
+#include <map>
+#include <boost/asio.hpp>
+#include "CSession.h"
+using boost::asio::ip::tcp;
+
+class CServer {
+public:
+    CServer(boost::asio::io_context& ioc, short port);
+    void ClearSession(const std::string&);
+private:
+    void HandleAccept( std::shared_ptr<CSession>, const boost::system::error_code &error );
+    void StartAccept();
+    boost::asio::io_context &_ioc;
+    short _port;
+    tcp::acceptor _acceptor;
+    std::map<std::string, std::shared_ptr<CSession>> _sessions;
+};
