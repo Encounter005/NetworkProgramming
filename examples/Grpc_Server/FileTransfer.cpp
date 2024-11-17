387e4290d0f7910796744cfe01521f30dd08af60
encounter005 <1624221574@qq.com>
2024-11-17 20:38:57 +0800

    ADD: filetransfer

---
+++ examples/Grpc_Server/FileTransfer.cpp
@@ -0,0 +1,102 @@
+#include "../../out/Debug/proto/gen_cxx/demo.grpc.pb.h"
+#include <fstream>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server_context.h>
+#include <iostream>
+#include <memory>
+#include <string>
+
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReader;
+using grpc::Status;
+using hello::Chunk;
+using hello::Greeter;
+using hello::HelloReply;
+using hello::HelloRequest;
+
+
+class GreeterServiceImpl final : public Greeter::Service {
+    Status SayHello(
+        ServerContext *context, const HelloRequest *request,
+        HelloReply *reply) override {
+        std::cout << "Connect a client from: " << context->peer() << " message: " << request->message() << std::endl;
+        std::string prefix("Server received: ");
+        reply->set_message(prefix + request->message());
+        return Status::OK;
+    }
+
+    Status Upload(
+        ServerContext *context, ServerReader<Chunk> *reader,
+        hello::Reply *reply) override {
+        Chunk         chunk;
+        std::ofstream outputfile;
+        const char   *data;
+        bool          bRead = reader->Read(&chunk);
+        if (!bRead) {
+            reply->set_length(0);
+            return Status::OK;
+        }
+
+        std::string filename = chunk.filename();
+        std::string output_folder = "./test_output_files/";
+        std::string output_path = output_folder + filename;
+
+
+        // NOTE:
+        //  - std::ostream::out
+        //      是一个打开模式标志，表示以输出模式打开文件。
+        //      文件将被打开以便写入数据。如果文件不存在，则会创建一个新文件。
+        //      如果文件已经存在，文件内容将被清空。
+        //  - std::ofstream::app
+        //      这是一个打开模式标志，表示以追加模式打开文件。
+        //      文件将被打开以便写入数据，但所有写入操作将在文件末尾进行，不会覆盖文件的现有内容。
+        //  - std::ofstream::binary
+        //      这是一个打开模式标志，表示以二进制模式打开文件。
+        //      文件将被打开以便进行二进制数据读写操作，而不是文本数据读写操作。
+        // 可以用管道符将多个打开模式组合在一起
+        //      以输出模式打开文件 filename。
+        //      以追加模式打开文件，所有写入操作将在文件末尾进行。
+        //      以二进制模式打开文件，进行二进制数据读写操作。
+        outputfile.open(
+            output_path,
+            std::ostream::out | std::ofstream::app | std::ofstream::binary);
+
+        if (outputfile.is_open()) {
+            std::cout << "File: " << filename << " open success" << std::endl;
+        } else {
+            std::cout << "File: " << filename << " open failed" << std::endl;
+        }
+
+        while (bRead && !(chunk.buffer()).empty()) {
+            data = chunk.buffer().c_str();
+            outputfile.write(data, chunk.buffer().length());
+            bRead = reader->Read(&chunk);
+        }
+
+        int64_t pos = outputfile.tellp();
+        reply->set_length(static_cast<int32_t>(pos));
+        outputfile.close();
+        return Status::OK;
+    }
+};
+
+void RunServer() {
+    std::string        server_address("127.0.0.1:50001");
+    GreeterServiceImpl service;
+    ServerBuilder      builder;
+
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+    server->Wait();
+}
+
+int main(int argc, char *argv[]) {
+    RunServer();
+    return 0;
+}
---
+++ examples/Grpc_Server/Grpc_Client.cpp
@@ -0,0 +1,137 @@
+#include "../../out/Debug/proto/gen_cxx/demo.grpc.pb.h"
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <fstream>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/security/credentials.h>
+#include <grpcpp/security/server_credentials.h>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::ClientWriter;
+using grpc::Status;
+using hello::Chunk;
+using hello::Greeter;
+using hello::HelloReply;
+using hello::HelloRequest;
+using hello::Reply;
+
+constexpr int CHUNK_SIZE = 1024 * 1024;
+
+class FCClient {
+public:
+    FCClient(std::shared_ptr<Channel> channel);
+    std::string SayHello(const std::string &name);
+    void        PathToUpload(const std::string &path);
+
+private:
+    void                           Upload(const std::string &filepath);
+    std::unique_ptr<Greeter::Stub> stub_;
+};
+
+
+FCClient::FCClient(std::shared_ptr<Channel> channel)
+    : stub_(Greeter::NewStub(channel)) {}
+
+std::string FCClient::SayHello(const std::string &name) {
+    ClientContext client_context;
+    HelloReply    reply;
+    HelloRequest  request;
+    request.set_message(name);
+    Status status = stub_->SayHello(&client_context, request, &reply);
+
+    if (status.ok()) {
+        return reply.message();
+    } else {
+        return "failure " + status.error_message();
+    }
+}
+
+void FCClient::Upload(const std::string &filepath) {
+    Chunk         chunk;
+    char          data[CHUNK_SIZE];
+    Reply         stats;
+    ClientContext client_context;
+    std::ifstream inputfile;
+    int           len = 0;
+
+    std::string::size_type pos = filepath.find_last_of('/') + 1;
+    std::string filename       = filepath.substr(pos, filepath.length() - pos);
+
+
+    inputfile.open(filepath, std::ifstream::in | std::ifstream::binary);
+
+    if (inputfile.is_open()) {
+        std::cout << "File: " << filename << " open success" << std::endl;
+    } else {
+        std::cout << "File: " << filename << " open failed" << std::endl;
+        throw "File open failed";
+    }
+
+    std::unique_ptr<ClientWriter<Chunk>> writer(
+        stub_->Upload(&client_context, &stats));
+
+    while (!inputfile.eof()) {
+        inputfile.read(data, CHUNK_SIZE);
+        chunk.set_buffer(data, inputfile.gcount());
+        chunk.set_filename(filename);
+        if (!writer->Write(chunk)) {
+            break;
+        }
+        len += inputfile.gcount();
+    }
+
+    writer->WritesDone();
+    Status status = writer->Finish();
+    if (status.ok()) {
+        std::cout << "Transfer " << filename << " success, " << len << " bytes"
+                  << std::endl;
+    } else {
+        std::cout << "Transfer " << filename << " failed, "
+                  << status.error_message() << std::endl;
+    }
+    inputfile.close();
+}
+
+
+void FCClient::PathToUpload(const std::string &path) {
+    boost::filesystem::path dir_path(path);
+
+    if (!boost::filesystem::exists(dir_path)
+        || !boost::filesystem::is_directory(dir_path)) {
+        throw std::runtime_error(
+            "Directory does not exist or is not a directory: " + path);
+    }
+
+    for (const auto &entry : boost::filesystem::directory_iterator(dir_path)) {
+        this->Upload(entry.path().string());
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    std::string server_address("127.0.0.1:50001");
+    auto        channel = grpc::CreateChannel(
+        server_address, grpc::InsecureChannelCredentials());
+
+
+    try {
+
+        FCClient client(channel);
+        client.SayHello("I will transfer files");
+
+        std::string path = "./test_input_files";
+        client.PathToUpload(path);
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    return 0;
+}
